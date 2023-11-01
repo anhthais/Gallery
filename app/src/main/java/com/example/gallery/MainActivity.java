8c601251fe533c940757daf4c965156fe02e468a
DELL <nguyenquocvu200333002@gmail.com>
2023-11-01 18:46:03 +0700

    Merge branch 'master' into load-images

--- app/src/main/java/com/example/gallery/MainActivity.java
+++ app/src/main/java/com/example/gallery/MainActivity.java
@@ -1,38 +1,41 @@
 package com.example.gallery;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.Manifest;
-import android.app.Activity;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
+import android.app.ActionBar;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.gallery.Database.DatabaseHelper;
+import com.example.gallery.fragment.AlbumFragment;
 import com.example.gallery.fragment.GalleryFragment;
+import com.example.gallery.object.Album;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity implements MainCallBacks {
 
-public class MainActivity extends AppCompatActivity {
+
     private static final int PERMISSION_REQUEST_READ_CODE = 1;
+
+    FragmentTransaction ft;
+    Menu menu;
+    GalleryFragment gallery_fragment=null;
+    AlbumFragment album_fragment=null;
     BottomNavigationView btnv;
-    DatabaseHelper galleryDB;
-    ArrayList<String> image_id, image_name, image_address, album_name, image_status, image_timeRemaining;
+    ActionBar action_bar;
+    public BottomNavigationView getNavigationBar(){
+        return this.btnv;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,15 +45,42 @@ public class MainActivity extends AppCompatActivity {
         btnv.setOnNavigationItemSelectedListener(item -> {
             int id=item.getItemId();
             if(id==R.id.btnGallery){
-
+                menu.findItem(R.id.btnAddNewAlbum).setVisible(false);
+                if(this.gallery_fragment!=null){
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.mainFragment, gallery_fragment); ft.commit();
+                }else{
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    GalleryFragment galleryFragment = new GalleryFragment(MainActivity.this);
+                    ft.replace(R.id.mainFragment, galleryFragment); ft.commit();
+                }
             }
             else if (R.id.btnAlbum==id){
+                menu.findItem(R.id.btnAddNewAlbum).setVisible(true);
+                if(this.album_fragment!=null){
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.mainFragment, album_fragment); ft.commit();
+                }
+                else {
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ArrayList<Album> al = new ArrayList<Album>();
+                    al.add(new Album("Album 1"));
+                    al.add(new Album("Album 2"));
+                    al.add(new Album("Album 3"));
+                    al.add(new Album("Album 4"));
+                    al.add(new Album("Album 5"));
+                    AlbumFragment album = new AlbumFragment(this, al);
+                    album_fragment = album;
+                    ft.replace(R.id.mainFragment, album);
+                    ft.commit();
+                }
 
             }
             else if (R.id.btnSettings==id){
                 View v=findViewById(R.id.btnSettings);
                 PopupMenu pm = new PopupMenu(this, v);
                 pm.getMenuInflater().inflate(R.menu.settings_menu, pm.getMenu());
+
                 pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
@@ -60,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
-
         // request permission
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
             ActivityCompat.requestPermissions(MainActivity.this,
@@ -93,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
     private void initApp(){
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         GalleryFragment galleryFragment = new GalleryFragment(MainActivity.this);
+        this.gallery_fragment=galleryFragment;
         ft.replace(R.id.mainFragment, galleryFragment); ft.commit();
     }
 
@@ -111,34 +141,31 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Permission denied!", Toast.LENGTH_SHORT).show();
             }
         }
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.actionbar_menu,menu);
+        menu.findItem(R.id.btnAddNewAlbum).setVisible(false);
+        this.menu=menu;
         return super.onCreateOptionsMenu(menu);
     }
 
-    void storeDataInArrays(){
-        Cursor cursor = galleryDB.readAllData();
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.btnAddNewAlbum){
+            album_fragment.addNewAlbum();
+        }else if(id==R.id.btnAddImage){
 
-        if(cursor.getCount() ==0 ){
-            Toast.makeText(this, "No data",Toast.LENGTH_SHORT).show();
         }
-        else{
-            while(cursor.moveToNext()){
-                image_id.add(cursor.getString(0));
-                image_name.add(cursor.getString(1));
-                image_address.add(cursor.getString(2));
-                album_name.add(cursor.getString(3));
-                image_status.add(cursor.getString(4));
-                image_timeRemaining.add(cursor.getString(5));
-
-            }
-        }
+        return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onMsgFromFragToMain(String sender, String strValue) {
 
-    public void onMsgFromFragToMain(String album, String name) {
     }
+
 }
